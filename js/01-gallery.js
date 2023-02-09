77a8814ac6e7d5e97c2ba2c344a24c24a17caa8d
fxhxyz4 <fxhxyz@proton.me>
2023-02-09 05:03:26 +0200

    -

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,58 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+/*
+    Создай галерею с возможностью клика по её элементам и просмотра
+    полноразмерного изображения в модальном окне.
+*/
 
+console.clear();
+
+import { galleryItems } from "./gallery-items.js";
+const galleryEl = document.querySelector(".gallery");
+const itemEl = createMarkup(galleryItems);
 console.log(galleryItems);
+
+// функция для перебора "galleryItems" и создания "div" элемента
+function createMarkup() {
+	return galleryItems
+		.map(({ preview, original, description }) => {
+			return `
+                <div class="gallery__item">
+                    <a href="${original}" class="gallery__link">
+                        <img
+                            src="${preview}"
+                            data-source="${original}"
+                            alt="${description}"
+                            class="gallery__image"
+                        />
+                    </a>
+                </div>`;
+		})
+		.join(" ");
+}
+
+// добавление "galleryItems" в html
+galleryEl.insertAdjacentHTML("beforeend", itemEl);
+
+// слушатель события для "galleryEl"
+galleryEl.addEventListener("click", onClick);
+
+// функция сравнения и "keydown"
+function onClick(e) {
+	e.preventDefault();
+	if (e.target.nodeName !== "IMG" && !document.querySelector(".basicLightbox")) {
+		return;
+	}
+	const src = e.target.dataset.source;
+	let width = 800;
+	let height = 600;
+
+	const imgCrt = basicLightbox.create(`
+        <img src="${src} "width="${width}" height="${height}">
+    `);
+	imgCrt.show();
+
+	document.addEventListener("keydown", (e) => {
+		if (e.code === "Escape") {
+			imgCrt.close();
+		}
+	});
+}
