08978031c24073e0436abe0e65c3bda81e87df4a
fxhxyz4 <fxhxyz@proton.me>
2023-02-09 05:07:20 +0200

    -

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,7 @@
-import { galleryItems } from './gallery-items.js';
+console.clear();
+
+import { galleryItems } from "./gallery-items.js";
+const galleryEl = document.querySelector(".gallery");
 // Change code below this line
 
-console.log(galleryItems);
+console.log(galleryEl);
