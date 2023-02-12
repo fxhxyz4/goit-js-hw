d560bc83b34e9d646280c29b55493c79f82a4427
fxhxyz4 <fxhxyz@proton.me>
2023-02-12 18:35:31 +0200

    edit 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,46 @@
+console.clear();
+
+import { Notify } from 'notiflix';
+
+const form = document.querySelector('.form');
+// const input = document.querySelectorAll('input[type=number]');
+const inputDelay = document.querySelector('input[name=delay');
+const inputStep = document.querySelector('input[name=step]');
+const inputAmount = document.querySelector('input[name=amount]');
+const btn = document.querySelector('button[type=submit]');
+
+form.addEventListener('submit', e => {
+  e.preventDefault();
+});
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
 }
+
+btn.addEventListener('click', e => {
+  let delay = Number(inputDelay.value);
+  let delayStep = Number(inputStep.value);
+  let delayAmount = Number(inputAmount.value);
+
+  for (let i = 0; i < delayAmount; i++) {
+    createPromise(i, delay)
+      .then(({ position, delay }) => {
+        Notify.success(`success promise ${position}, delay ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        Notify.failure(`failure promise ${position}, delay ${delay}ms`);
+      });
+  }
+
+  delay += delayStep;
+});
