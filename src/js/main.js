683441eaac844401e85d910ff3b0f699b2a48f94
fxhxyz4 <fxhxyz@proton.me>
2023-03-06 20:58:54 +0200

    -

--- src/js/main.js
+++ src/js/main.js
@@ -1,15 +1,11 @@
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import fetchApi from './components/fetchImg';
-import loadmore from './components/loadmore';
 import { renderHtml, addHtml } from './components/render';
 
 const form = document.querySelector('.search-form');
 const gallery = document.querySelector('.gallery');
+const loadMore = document.querySelector('.load-more');
 const apiService = new fetchApi();
-const loadMoreBtn = new loadmore({
-  selector: '.load-more',
-  isHidden: true,
-});
 
 function clearHtml() {
   gallery.innerHTML = '';
@@ -25,34 +21,55 @@ form.addEventListener('submit', e => {
   clearHtml();
   apiService.resetPage();
 
-  loadMoreBtn.show();
   fetchData().finally(() => form.reset());
+
+  loadMore.classList.remove('is-hidden');
 });
 
 function fetchData() {
-  loadMoreBtn.disable();
   let currentHits = apiService.currentHits();
 
   return apiService
     .fetchImg()
     .then(({ hits, totalHits }) => {
       if (!hits.length) {
-        loadMoreBtn.hide();
         Notify.failure(
           'Sorry, there are no images matching your search query. Please try again.'
         );
+
+        loadMore.classList.add('is-hidden');
         return;
       }
       renderHtml(hits);
 
       if (currentHits >= totalHits) {
-        loadMoreBtn.hide();
         Notify.info(
           "We're sorry, but you've reached the end of search results."
         );
+        loadMore.classList.add('is-hidden');
         return '';
       }
-      loadMoreBtn.enable();
     })
     .catch(console.log);
 }
+
+//fix
+
+loadMore.addEventListener('click', async e => {
+  if (apiService.displayAmount >= apiService.myTotalHits) {
+    Notify.warning(
+      "We're sorry, but you've reached the end of search results."
+    );
+    loadMore.classList.add('is-hidden');
+    return;
+  }
+  apiService.displayAmount += apiService.per_page;
+
+  try {
+    const fetchedData = await apiService.fetchArticles();
+    const { hits: articles } = fetchedData;
+    appendArticlesMarkup(articles);
+  } catch (err) {
+    console.error(err.message);
+  }
+});
